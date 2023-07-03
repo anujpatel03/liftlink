f3bbfaaeea2a13989853f4424ee451153a241c23
Anuj Patel <anujkpatel4@gmail.com>
2023-07-04 02:18:28 +0530

    Added Map

--- src/app/components/Map.js
+++ src/app/components/Map.js
@@ -1,10 +1,37 @@
+"use client";
 
-function Map() {
-  return (
-    <div>
-        <h1>Map</h1>
-    </div>
-  )
+import { useEffect} from 'react'
+import mapboxgl from 'mapbox-gl'
+
+
+
+const styles = {
+    wrapper: `w-full h-full flex-1`,
 }
 
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
+
+const Map = () => {
+    useEffect(() => {
+        const map = new mapboxgl.Map({
+            container: 'map',
+            // style: 'mapbox://styles/mapbox/streets-v11',
+            style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph',
+            center: [-74.5, 40],
+            zoom: 9
+        })
+    }, [])
+
+
+    return (
+        <div className={styles.wrapper}>
+            <div id="map" className="w-full h-full">
+                <Map />
+            </div>
+        </div>
+    )
+}
+
+
 export default Map
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Navbar from './components/Navbar'
-import Map from './components/Map'
+import dynamic from "next/dynamic";
+// import Map from './components/Map'
 
 const style = {
   wrapper: 'flex flex-col h-screen w-screen',
@@ -8,13 +9,17 @@ const style = {
   rideRequest: 'flex flex-col items-center justify-center flex-1 px-20 text-center',
 }
 export default function Home() {
+  const MapWithNoSSR = dynamic(() => import("./components/Map"), {
+    ssr: false
+  });
   return (
     <div className={style.wrapper}>
       <Navbar />
       {/* navbar */}
       <div className={style.main}>
         {/* map */}
-        <Map />
+        {/* <Map /> */}
+        <MapWithNoSSR />
       </div>
       <div className={style.rideRequestContainer}>
         <div className={style.rideRequest}>

1a9726f25aa6d5e6c18f66c9c1d2d40a88130a23
Anuj Patel <anujkpatel4@gmail.com>
2023-07-04 08:01:38 +0530

    changes in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Navbar from './components/Navbar'
 import dynamic from "next/dynamic";
-// import Map from './components/Map'
+import Map from './components/Map'
 
 const style = {
   wrapper: 'flex flex-col h-screen w-screen',
@@ -9,17 +9,18 @@ const style = {
   rideRequest: 'flex flex-col items-center justify-center flex-1 px-20 text-center',
 }
 export default function Home() {
-  const MapWithNoSSR = dynamic(() => import("./components/Map"), {
-    ssr: false
-  });
+  // const MapWithNoSSR = dynamic(() => import("./components/Map"), {
+  //   ssr: false
+  // });
+
   return (
     <div className={style.wrapper}>
       <Navbar />
       {/* navbar */}
       <div className={style.main}>
         {/* map */}
-        {/* <Map /> */}
-        <MapWithNoSSR />
+        <Map />
+        {/* <MapWithNoSSR /> */}
       </div>
       <div className={style.rideRequestContainer}>
         <div className={style.rideRequest}>
